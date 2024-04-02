c77cf22be7f848a1cbd15d4bf47f004c4339c9f5
Venkataramana Pakeeru <venkat@venkatamanasair.lan>
2024-04-02 18:33:03 -0400

    initial code

---
+++ staging/main.tf
@@ -0,0 +1,24 @@
+
+resource "aws_instance" "appserver" {
+  ami           = "ami-019f9b3318b7155c5"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "${var.environment}-appserver"
+  }
+}
+
+resource "aws_instance" "dbserver" {
+  ami           = "ami-019f9b3318b7155c5"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "${var.environment}-dbserver"
+  }
+}
+
+resource "aws_instance" "dbserver1" {
+  ami           = "ami-019f9b3318b7155c5"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "${var.environment}-dbserver1"
+  }
+}
