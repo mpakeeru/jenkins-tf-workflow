845604b224603e076656fb63ef2f4a680d81a731
mpakeeru <mamathasama@hotmail.com>
2024-04-03 15:23:02 -0400

    deleted dbserver

--- dev/main.tf
+++ dev/main.tf
@@ -7,19 +7,3 @@ resource "aws_instance" "appserver" {
   }
 }
 
-resource "aws_instance" "dbserver" {
-  ami           = "ami-019f9b3318b7155c5"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "${var.environment}-dbserver"
-  }
-}
-
-
-resource "aws_instance" "dbserver1" {
-  ami           = "ami-019f9b3318b7155c5"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "${var.environment}-dbserver1"
-  }
-}
