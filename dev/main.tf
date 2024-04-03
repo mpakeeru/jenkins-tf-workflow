8bab4b8fedcc1d3a744bce8b7975611a74819f58
mpakeeru <mamathasama@hotmail.com>
2024-04-03 14:38:21 -0400

    removed web servers

--- dev/main.tf
+++ dev/main.tf
@@ -16,17 +16,3 @@ resource "aws_instance" "dbserver" {
 }
 
 
-resource "aws_instance" "web_server1" {
-  ami           = "ami-019f9b3318b7155c5"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "${var.environment}-web-server1"
-  }
-}
-resource "aws_instance" "web_server2" {
-  ami           = "ami-019f9b3318b7155c5"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "${var.environment}-web-server2"
-  }
-}
