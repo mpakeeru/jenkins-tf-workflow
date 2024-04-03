ad350ae121d199c6fec1a7a593bad4dc38961907
mpakeeru <mamathasama@hotmail.com>
2024-04-03 15:34:19 -0400

    deleted dbserver

--- dev/main.tf
+++ dev/main.tf
@@ -1,9 +1,9 @@
 
-resource "aws_instance" "appserver" {
+resource "aws_instance" "appserver2" {
   ami           = "ami-019f9b3318b7155c5"
   instance_type = "t2.micro"
   tags = {
-    Name = "${var.environment}-appserver"
+    Name = "${var.environment}-appserver2"
   }
 }
 
