d7422b3e0632065e4ee53c6389c4094c95179b10
mpakeeru <mamathasama@hotmail.com>
2024-04-03 11:22:29 -0400

    webserver2

--- dev/main.tf
+++ dev/main.tf
@@ -30,3 +30,10 @@ resource "aws_instance" "web_server1" {
     Name = "${var.environment}-web-server1"
   }
 }
+resource "aws_instance" "web_server2" {
+  ami           = "ami-019f9b3318b7155c5"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "${var.environment}-web-server2"
+  }
+}
